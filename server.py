3e70b22add6bf3e36b12fc55c9304df2cbd4ab91
claudiarivasalas <claudia.rivas.salas@gmail.com>
2022-10-03 22:50:04 -0300

    Tabla Html

---
+++ server.py
@@ -0,0 +1,5 @@
+from tabla_html_app import app
+from tabla_html_app.controllers import controlador 
+
+if __name__=="__main__":   # Asegúrate de que este archivo se esté ejecutando directamente y no desde un módulo diferente    
+    app.run(debug=True)    # Ejecuta la aplicación en modo de depuración
---
+++ tabla_html_app/controllers/controlador.py
@@ -0,0 +1,19 @@
+from tabla_html_app import app
+from flask import render_template, request, redirect
+
+@app.route('/users')
+def lista_users():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template("tabla.html", usuarios = users)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return "ESTA RUTA NO FUE ENCONTRADA", 404
+    #return render_template('404.html'), 404
+
